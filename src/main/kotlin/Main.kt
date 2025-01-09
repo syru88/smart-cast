ffc1d5d11b14ada0ca81b7755b85a71585d613a8
Marcel Syrucek <marcel.syrucek@gmail.com>
2025-01-09 21:38:30 +0100

    Better structure

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,31 @@
 package dev.marsellus
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import dev.marsellus.model.Description
 import dev.marsellus.model.Details
 import dev.marsellus.model.Pet
 
 fun main() {
 
-    val person = Person(name = "Marsellus", age = 25, details = "details", /*other = "other"*/)
-    val pet = Pet(id = 1L, name = "Pet", details = Details(description = Description("other information")))
+    val person = Person(
+        name = "Marsellus",
+        age = 25, details = "details",
+        other = "another"
+    )
+    val pet = Pet(id = 1L, name = "Pet", details = Details(description = "other information"))
 
     println("Hello $person!")
     requireNotNull(pet.details)
-    println("Hello ${pet.details.description}")
+    log(pet.details.description)
 }
 
-data class Person(@get:JsonProperty("name") val name: String, val age: Int, val details: String, /*val other: String*/)
+private fun log(value: String) {
+    println("log: $value")
+}
+
+data class Person(
+    @get:JsonProperty("name")
+    val name: String,
+    val age: Int,
+    val details: String,
+    val other: String,
+)
