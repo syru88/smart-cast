3bfa07dd0024a7c83df7715188fc30f6beaee36d
Marcel Syrucek <marcel.syrucek@gmail.com>
2025-01-09 21:09:25 +0100

    Open api

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     kotlin("jvm") version "2.0.21"
+
+    id("org.openapi.generator") version "7.10.0"
 }
 
 group = "dev.marsellus"
@@ -19,4 +23,31 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(21)
+}
+
+val openApi = tasks.withType<GenerateTask> {
+    generatorName.set("kotlin")
+    inputSpec.set("$projectDir/src/main/resources/schema.yaml")
+    configOptions.set(mapOf("serializationLibrary" to "jackson"))
+
+    generateModelDocumentation.set(false)
+    generateModelTests.set(false)
+    generateApiDocumentation.set(false)
+
+    // Generates all models, excludes APIs and support files (unless named in their *ConstrainedTo)
+    modelFilesConstrainedTo.set(listOf(""))
+
+    modelPackage.set("dev.marsellus.model")
+
+    sourceSets {
+        main {
+            kotlin {
+                srcDir(layout.buildDirectory.dir("generate-resources/main/src/main/kotlin"))
+            }
+        }
+    }
+}
+
+tasks.named("compileKotlin") {
+    dependsOn(openApi)
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,16 @@
 package dev.marsellus
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import org.openapitools.client.models.Details
+import org.openapitools.client.models.Pet
 
 fun main() {
 
-    val person = Person(name = "Marsellus", age = 25)
+    val person = Person(name = "Marsellus", age = 25, details = "other info")
+    val pet = Pet(id = 1L, name = "Pet", details = Details(description = "other information"))
 
     println("Hello $person!")
+    println("Hello ${pet.details.description}")
 }
 
-data class Person(@get:JsonProperty("name") val name: String, val age: Int, val gender: String = "Male")
+data class Person(@get:JsonProperty("name") val name: String, val age: Int, val details: String, val other: String = "other")
