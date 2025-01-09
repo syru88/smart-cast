a052f6fb9de261286b46eae3ba9760580b81353c
Marcel Syrucek <marcel.syrucek@gmail.com>
2025-01-09 21:16:30 +0100

    Complex model

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,17 @@
 package dev.marsellus
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import org.openapitools.client.models.Details
-import org.openapitools.client.models.Pet
+import dev.marsellus.model.Description
+import dev.marsellus.model.Details
+import dev.marsellus.model.Pet
 
 fun main() {
 
-    val person = Person(name = "Marsellus", age = 25, details = "other info")
-    val pet = Pet(id = 1L, name = "Pet", details = Details(description = "other information"))
+    val person = Person(name = "Marsellus", age = 25, details = "details", other = "other")
+    val pet = Pet(id = 1L, name = "Pet", details = Details(description = Description("other information")))
 
     println("Hello $person!")
     println("Hello ${pet.details.description}")
 }
 
-data class Person(@get:JsonProperty("name") val name: String, val age: Int, val details: String, val other: String = "other")
+data class Person(@get:JsonProperty("name") val name: String, val age: Int, val details: String, val other: String)
