ec27616877dd832d1c74d11479f646a2e6b8ce60
Marcel Syrucek <marcel.syrucek@gmail.com>
2025-01-09 21:28:41 +0100

    Compile issue

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,11 +7,12 @@ import dev.marsellus.model.Pet
 
 fun main() {
 
-    val person = Person(name = "Marsellus", age = 25, details = "details", other = "other")
+    val person = Person(name = "Marsellus", age = 25, details = "details", /*other = "other"*/)
     val pet = Pet(id = 1L, name = "Pet", details = Details(description = Description("other information")))
 
     println("Hello $person!")
+    requireNotNull(pet.details)
     println("Hello ${pet.details.description}")
 }
 
-data class Person(@get:JsonProperty("name") val name: String, val age: Int, val details: String, val other: String)
+data class Person(@get:JsonProperty("name") val name: String, val age: Int, val details: String, /*val other: String*/)
